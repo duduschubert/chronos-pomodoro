89600213c61d28b49d569d6a1c6570b5c65c655d
Eduardo Schubert <duduschubert@email.com>
2026-01-12 23:37:37 -0300

    Modificações 12/01/2026

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { Container } from './components/Container/index.tsx';
 import { Menu } from './components/Menu/Index.tsx'
 import { Logo } from './components/Logo/Index.tsx'
 import { CountDown } from './components/CountDown/Index.tsx';
+import { DefaultInput } from './components/DefaultInput/Index.tsx';
+import { Cycles } from './components/Cycles/Index.tsx';
+import { DefaultButton } from './components/DefaultButton/Index.tsx';
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer/index.tsx';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -19,6 +24,30 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+      
+      <Container>
+        <form className='form' action="">
+        
+        <DefaultInput labelText='Task/Tarefa' type='text' id='meuInput' placeholder='Digite algo'/>  
+        
+        <div className='formRow'> <p>Lorem ipsum dolor sit.</p>
+        </div>
+        
+        <div className='formRow'>
+          <Cycles />
+        </div>
+
+        <div className='formRow'>
+          <DefaultButton icon={<PlayCircleIcon/>} color='green'/>
+          <DefaultButton icon={<StopCircleIcon/>} color='red'/>
+        </div>
+
+          <div>
+            <Footer />
+          </div>
+
+        </form>
       </Container>
     </>
   );
---
+++ src/components/Cycles/Index.tsx
@@ -0,0 +1,22 @@
+import styles from './styles.module.css';
+
+export function Cycles() {
+  return (
+    <>
+      <div className={styles.cycles}>
+        <span>Ciclos:</span>
+
+      <div className={styles.cycleDots}>
+        <span className={`${styles.workTime} ${styles.cycleDot}`}></span>
+        <span className={`${styles.shortBreakTime} ${styles.cycleDot}`}></span>
+        <span className={`${styles.workTime} ${styles.cycleDot}`}></span>
+        <span className={`${styles.shortBreakTime} ${styles.cycleDot}`}></span>
+        <span className={`${styles.workTime} ${styles.cycleDot}`}></span>
+        <span className={`${styles.shortBreakTime} ${styles.cycleDot}`}></span>
+        <span className={`${styles.workTime} ${styles.cycleDot}`}></span>
+        <span className={`${styles.longBreakTime} ${styles.cycleDot}`}></span>
+        </div>
+      </div>
+    </>
+  );
+}
