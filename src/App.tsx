bf35cc23906e396bf3ad3173da0f535ea249880f
Eduardo Schubert <duduschubert@email.com>
2026-01-14 21:12:10 -0300

    Modificações 14/01/2026

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
+import { Menu } from './components/Menu/Index.tsx';
+import { Logo } from './components/Logo/Index.tsx';
+import { Cycles } from './components/Cycles/Index.tsx';
+import { Footer } from './components/Footer/index.tsx';
 import { Container } from './components/Container/index.tsx';
-import { Menu } from './components/Menu/Index.tsx'
-import { Logo } from './components/Logo/Index.tsx'
 import { CountDown } from './components/CountDown/Index.tsx';
 import { DefaultInput } from './components/DefaultInput/Index.tsx';
-import { Cycles } from './components/Cycles/Index.tsx';
 import { DefaultButton } from './components/DefaultButton/Index.tsx';
 import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer/index.tsx';
+
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
+ 
+
   return (
     <>
       <Container>
         <Logo />
-        </Container>
-            
+      </Container>
+
       <Container>
         <Menu />
       </Container>
@@ -25,28 +28,33 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
-      
-      <Container>
-        <form className='form' action="">
-        
-        <DefaultInput labelText='Task/Tarefa' type='text' id='meuInput' placeholder='Digite algo'/>  
-        
-        <div className='formRow'> <p>Lorem ipsum dolor sit.</p>
-        </div>
-        
-        <div className='formRow'>
-          <Cycles />
-        </div>
 
-        <div className='formRow'>
-          <DefaultButton icon={<PlayCircleIcon/>} color='green'/>
-          <DefaultButton icon={<StopCircleIcon/>} color='red'/>
-        </div>
+      <Container>
+        <form className='form' action=''>
+          <DefaultInput
+            labelText='Task/Tarefa'
+            type='text'
+            id='meuInput'
+            placeholder='Digite algo'
+          />
+
+          <div className='formRow'>
+            {' '}
+            <p>Lorem ipsum dolor sit.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles />
+          </div>
+
+          <div className='formRow'>
+            <DefaultButton icon={<PlayCircleIcon />} color='green' />
+            <DefaultButton icon={<StopCircleIcon />} color='red' />
+          </div>
 
           <div>
             <Footer />
           </div>
-
         </form>
       </Container>
     </>
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,14 @@
-import styles from './styles.module.css'
+import styles from './styles.module.css';
 
 export function Footer() {
-  return (<>
-  <footer className={styles.footer}>
-   <a href="">Entenda a técnica de pomodoro 🍅</a>
-    <a href="">Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com o 💚</a></footer>
+  return (
+    <>
+      <footer className={styles.footer}>
+        <a href=''>Entenda a técnica de pomodoro 🍅</a>
+        <a href=''>
+          Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com o 💚
+        </a>
+      </footer>
     </>
-  )
+  );
 }
