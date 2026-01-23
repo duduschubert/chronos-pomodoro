cf6ebd48f7f10cac7a88833f3a8e3409cb41993a
Eduardo Schubert <duduschubert@email.com>
2026-01-23 16:27:03 -0300

    Modificações 23/01/2026

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { Home } from './pages/Home';
-import { useState } from 'react';
-import { TaskContextProvider } from './contexts/TaskContext';
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
- const [state, setState] = useState();
-  
   return (
   <TaskContextProvider>
     <Home/>;
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,25 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles/Index';
 import { DefaultButton } from '../DefaultButton/Index';
 import { DefaultInput } from '../DefaultInput/Index';
+import { useRef } from 'react';
 
 export function MainForm() {
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log('Deu certo');
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='form'>
         <DefaultInput
           labelText='Task/Tarefa'
           type='text'
           id='meuInput'
           placeholder='Digite algo'
+          ref = {taskNameInput}
         />
       </div>
 
